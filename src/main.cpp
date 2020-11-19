663260c07616a67eac79e1df6e9e86900674ac9e
vsensu <vsensu@foxmail.com>
2020-11-19 17:10:14 +0800

    Error handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include <string>
 
+bool had_error = false;
+
+void report(int line, const std::string &where, const std::string &message)
+{
+    std::cerr << "[line " << line << "] Error" << where << ": " << message << "\n";
+    had_error = true;
+}
+
+void error(int line, const std::string &message)
+{
+    report(line, "", message);
+}
+
 void run(const std::string &code)
 {
     std::cout << "code: " << code << "\n";
@@ -20,6 +33,7 @@ void run_prompt()
         std::getline(std::cin, line);
         if(line.size() == 0) break;
         run(line);
+        had_error = false;
     }
 }
 
